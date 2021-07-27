3a644bcec82b045da96c1e799a09ad79e5ca3146
hjhsitihamizah <40191586+hjhsitihamizah@users.noreply.github.com>
2021-07-27 16:43:10 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
-const apiData = {
-  url: 'https://pokeapi.co/api/v2/',
-  type: 'pokemon',
-  id: '25',
-}
+const inputName = document.getElementById.innerHTML
 
-const apiUrl = `${apiData.url}${apiData.type}/${apiData.id}`
-console.log(apiUrl)
+const apiData = {
+    url: 'https://pokeapi.co/api/v2/',
+    type: 'pokemon',
+    id: '24',
+}
+const { url, type, id } = apiData
+
+// const apiUrl = `${apiData.url}${apiData.type}/${apiData.id}` //this line same as below line
+const apiUrl = `${url}${type}/${id}`
+fetch(apiUrl)
+    .then((pokeData) => pokeData.json())
+    .then((pokemon) => generateHtml(pokemon))
+
+const generateHtml = (data) => {
+    console.log(data)
+    const html = `
+        <div class="name">${data.name}</div>
+        <img class="center" src=${data.sprites.front_default}>
+        <div class="details">
+            <span class="center">Height: ${data.height}</span>
+            <span class="center">Weight: ${data.weight}</span>
+        </div>
+    `
+    const pokemonDiv = document.querySelector('.pokemon')
+    pokemonDiv.innerHTML = html
+}
